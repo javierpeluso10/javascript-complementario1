e7d36d68f21dff49ce376625563af5f50c2d28b9
Javier Peluso <jpelusocoderhouse@gmail.com>
2022-07-12 10:41:43 -0300

    ejercicio complementario1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+let nombre = prompt("ingresar un nombre o escribe ESC para salir")
+
+while (nombre != "ESC") {
+
+    alert("el usuario ingreso el nombre de " + nombre);
+
+    nombre = prompt("ingresa otro nombre o escribe ESC para salir ");
+
+}
